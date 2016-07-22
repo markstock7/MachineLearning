1c756ab1601bea963bb1186d5c94fb410ecb3e78
mark stock <markstock7@hotmail.com>
2016-07-23 01:04:47 +0800

    knn

---
+++ kNN/kNN.py
@@ -0,0 +1,37 @@
+form numpy import *
+import operator
+
+def createDataSet(): 
+	group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1])
+    labels = ['A', 'A', 'B', 'B']
+    return group, lables
+
+def classify0(inX, dataSet, labels, k): 
+  	dataSetSize = dataSet.shape[0]
+	diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+	sqDiffMat = diffMat ** 2
+	sqDistances = sqDiffMat.sum(axis=1)
+	distances = sqDistances**0.5
+	sortedDistIndicies = distances.argsort()
+	classCount = {}
+	for i in range(k):
+		voteIlabel = labels[sortedDistIndicies[i]]
+		classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+	sortedClassCount = sorted(classCount.iteritems(),
+		key=operator.itemgetter(1), reverse=True)
+	return sortedClassCount[0][0]
+
+# 将文本纪录到转换NumPy的解析程序
+def file2matrix(filename):
+	fr = open(filename)
+	arrayOLines = fr.readlines()
+	numberOfLines = len(arrayOLines)
+	returnMat = zeros((numberOfLines, 3))
+	index = 0
+	for line in arrayOLines
+		line = line.strip()
+		listFromLine = line.split('\t')
+		returnMat[index, :] = listFromLine[0:3]
+		classLabelVector.append(int(listFromLine[-1]))
+		index += 1
+	return returnMat.classLabelVector
